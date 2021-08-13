9fb5ea976608323aaf8fd73d3144930cf66f1df4
Emma Andreea Chirlomez <s194780@win.dtu.dk>
2021-08-14 01:01:12 +0200

    a lof of stuff

---
+++ controller_cooling.py
@@ -0,0 +1,19 @@
+"""
+   Controls the cooling unit.
+"""
+
+from machine import Pin
+from time import sleep
+import utils_constants as constants
+
+_relay = Pin(constants.COOLER_PIN, Pin.OUT)
+_relay.value(0)
+is_started = False
+
+def start():
+  is_started = True
+  _relay.value(1)
+
+def stop():
+  is_started = False
+  _relay.value(0)
