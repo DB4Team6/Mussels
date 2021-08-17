9e20fc00dd025887eba437cf70ffb6961ff6d6a7
Emma Andreea Chirlomez <s194780@win.dtu.dk>
2021-08-17 21:22:28 +0200

    honhonhon

--- math_model.py
+++ math_model.py
@@ -7,20 +7,19 @@ Created on Fri Aug 13 10:57:50 2021
 
 #input is t_max which is time since last feeding 
 # and measured_Ca which is measured c of algea in cells/ml
-def feed_amount(t_max,measured_Ca) 
+def feed_amount(measured_Ca):
 
     #Questionable choices
-    dt = 1; #time step in min
+    dt = 1/3; #time step in min
     N_m = 1; #How many mussels?
+    t_max = 36 #Food every 12 min
 
     #Values from our data
-    mu_m = 0.011/60; #Growth constant of Algae in mussel tank [min^-1]
+    mu_m = 0.012/60; #Growth constant of Algae in mussel tank [min^-1]
     emu_m = 2.718**(mu_m*dt); #Another constant
     #Fm = 3*1000/60; #Feeding rate of a mussel in mL/min
-    min_Cm = 6000; #Concentration in algae tank cell/mL
-    max_Cm = 8000; #Concentration in mussel tank cell/mL
+    max_Cm = 7000; #Concentration in mussel tank cell/mL
     Vm = 2*1000; #Start volume in mussel tank [mL]
-    Va = 2*1000; #Start volume in algae tank [mL]
 
     #Initialize stuff
     Cm = [] #Simulated concentration in Mussel tank
@@ -33,8 +32,4 @@ def feed_amount(t_max,measured_Ca)
     Vr = Vm*(max_Cm-Cm[-1])/(measured_Ca-max_Cm) #Finding volume of bolus needed
 
     return Vr
-    return Cm #returns Volume in ml
 
-def initialise_algae(volume_a,volume_m):
-    #do the thing
-    return pump_amount
